d7834c58727cf83c65f5ad0c0c3093c4f5616882
Simon Gleizes <11026966+glzzzs@users.noreply.github.com>
2026-09-04 09:40:51 -0400

    [init] Add basic PyQt6 application with QLabel

--- src/eyedrop.py
+++ src/eyedrop.py
@@ -1,1 +1,12 @@
+import sys
 
+from PyQt6.QtWidgets import QApplication, QLabel
+
+
+def main() -> int:
+    app = QApplication(sys.argv)
+    window = QLabel("Hi, droppy 👁️")
+    window.setWindowTitle("eyedroppy")
+    window.resize(400, 300)
+    window.show()
+    return app.exec()
